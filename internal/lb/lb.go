921cae8f1da9db181cbadff2b449bd305b3bc683
Filippo Muscherà <filippo.muschera@alumni.uniroma2.eu>
2025-11-26 19:45:56 +0100

    Add dynamic memory update for proxy targets
    Introduce a custom ProxyConfig with ModifyResponse to update the available memory of each node after function execution, using custom headers. Also update current targets based on registry changes.

--- internal/lb/lb.go
+++ internal/lb/lb.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -32,8 +33,38 @@ func StartReverseProxy(e *echo.Echo, region string) {
 	log.Printf("Initializing with %d targets.\n", len(targets))
 	balancer := newBalancer(targets)
 	currentTargets = targets
-	e.Use(middleware.Proxy(balancer))
 
+	// Custom ProxyConfig to process custom headers and update available memory of each targets after they
+	// executed a function.
+	// These headers are set after the execution of the function on the target node, so the free memory already
+	// includes the memory freed by the function, once it's executed.
+	proxyConfig := middleware.ProxyConfig{
+		Balancer: balancer,
+
+		// We use ModifyResponse to process these headers
+		ModifyResponse: func(res *http.Response) error {
+
+			nodeName := res.Header.Get("Serverledge-Node-Name")
+			freeMemStr := res.Header.Get("Serverledge-Free-Mem")
+
+			if nodeName != "" && freeMemStr != "" {
+				freeMem, err := strconv.ParseInt(freeMemStr, 10, 64)
+				if err == nil {
+					NodeMetrics.Update(nodeName, freeMem, time.Now().Unix())
+
+					log.Printf("[LB-Update] Node %s reported %d MB free", nodeName, freeMem)
+				}
+			}
+
+			// Remove the no-longer-needed headers
+			res.Header.Del("Serverledge-Node-Name")
+			res.Header.Del("Serverledge-Free-Mem")
+
+			return nil
+		},
+	}
+
+	e.Use(middleware.ProxyWithConfig(proxyConfig))
 	go updateTargets(balancer, region)
 
 	portNumber := config.GetInt(config.API_PORT, 1323)
@@ -72,6 +103,7 @@ func updateTargets(balancer middleware.ProxyBalancer, region string) {
 		targets, err := getTargets(region)
 		if err != nil {
 			log.Printf("Cannot update targets: %v\n", err)
+			continue // otherwise we update everything with a nil target array, removing all targets from the LB list!
 		}
 
 		toKeep := make([]bool, len(currentTargets))
@@ -97,6 +129,13 @@ func updateTargets(balancer middleware.ProxyBalancer, region string) {
 			if !toKeep[i] {
 				log.Printf("Removing %s\n", curr.Name)
 				toRemove = append(toRemove, curr.Name)
+			} else {
+				// If we keep this node, then we'll update its info about free memory
+				nodeInfo := registration.GetSingleNeighborInfo(curr.Name)
+				if nodeInfo != nil {
+					freeMemoryMB := nodeInfo.TotalMemory - nodeInfo.UsedMemory
+					NodeMetrics.Update(curr.Name, freeMemoryMB, nodeInfo.LastUpdateTime)
+				}
 			}
 		}
 		for _, curr := range toRemove {
