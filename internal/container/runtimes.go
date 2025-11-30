d3228ec13eb682295834bbad39074eb4dffc7608
Filippo Muscherà <filippo.muschera@alumni.uniroma2.eu>
2025-11-30 19:05:18 +0100

    Add go125 runtime support
    This runtime will also support concurrency

--- internal/container/runtimes.go
+++ internal/container/runtimes.go
@@ -18,6 +18,7 @@ var RuntimeToInfo = map[string]RuntimeInfo{
 	"python314":  {"fmuschera/serverledge-python314", []string{"python", "/entrypoint.py"}, true, []string{X86, ARM}},
 	"nodejs17ng": {"fmuschera/serverledge-nodejs17ng", []string{}, false, []string{X86, ARM}},
 	"java21":     {"fmuschera/serverledge-java21", []string{}, false, []string{X86, ARM}},
+	"go125":      {"fmuschera/serverledge-go125", []string{"/entrypoint.sh"}, true, []string{X86, ARM}},
 }
 
 // CustomRuntimeToInfo Map to keep track of architectures compatible with each custom runtime image associated with a function registered
