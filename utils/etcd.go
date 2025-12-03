330987aee583d4a40d0947e04b5e605809b5a411
Filippo Muscherà <filippo.muschera@alumni.uniroma2.eu>
2025-12-03 20:00:35 +0100

    Remove higher message size limits
    Thanks to a better solution used in serverledge-cli and in container.go this is no longer needed: we can pass bigger source file via an http link instead of storing big files into etcd. (See commit be11a8ac395b8c7bc172129c5291c02cd7528b16).

--- utils/etcd.go
+++ utils/etcd.go
@@ -24,12 +24,9 @@ func GetEtcdClient() (*clientv3.Client, error) {
 
 	log.Println("Connecting to etcd")
 	etcdHost := config.GetString(config.ETCD_ADDRESS, "localhost:2379")
-	msgSizeLimit := 50 * 1024 * 1024 // Larger limit for fat jars and fat zip (java and go runtimes)
 	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:          []string{etcdHost},
-		DialTimeout:        3 * time.Second,
-		MaxCallSendMsgSize: msgSizeLimit,
-		MaxCallRecvMsgSize: msgSizeLimit,
+		Endpoints:   []string{etcdHost},
+		DialTimeout: 3 * time.Second,
 	})
 	if err != nil {
 		log.Printf("Could not connect to etcd: %v", err)
